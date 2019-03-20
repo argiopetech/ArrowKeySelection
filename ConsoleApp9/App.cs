65f7ddfd92b5c230a65ce03be2079d6573d24237
Elliot Robinson <elliot.robinson@erau.edu>
2019-03-20 10:57:47 -0400

    Add a basic up/down key list

---
+++ ConsoleApp9/App.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp9
+{
+    internal class App
+    {
+        internal void Run()
+        {
+            var options = new List<string> { "Option 1", "Option 2", "Option 3", "Option 4" };
+            var i = ArrowKeyOptionMenu("Choose an option:", options);
+
+            if (i != null)
+                Console.WriteLine($"Selected item {i}, value {options[(int)i]}");
+            else
+                Console.WriteLine("Bye...");
+        }
+
+        private int? ArrowKeyOptionMenu(string header, List<string> options)
+        {
+            var fg = Console.ForegroundColor;
+            var bg = Console.BackgroundColor;
+
+            void Highlight()
+            {
+                Console.ForegroundColor = bg;
+                Console.BackgroundColor = fg;
+            }
+
+            void ResetHighlight()
+            {
+                Console.ForegroundColor = fg;
+                Console.BackgroundColor = bg;
+            }
+
+            int selector = 0;
+            int count = options.Count;
+
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(header);
+
+                for (int i = 0; i < count; ++i)
+                {
+                    if (i == selector) Highlight();
+
+                    Console.WriteLine($"  {options[i]}");
+
+                    ResetHighlight();
+                }
+
+                var key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.DownArrow:
+                        selector++;
+                        selector %= count;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        selector--;
+                        selector = (selector + count) % count;
+                        break;
+                    case ConsoleKey.RightArrow:
+                    case ConsoleKey.Enter:
+                        return selector;
+                    case ConsoleKey.Q:
+                        return null;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ ConsoleApp9/XYCoord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class XYCoord
+    {
+        private int x;
+        private int y;
+
+        public XYCoord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        internal double DistanceTo(XYCoord dest)
+        {
+            var left = Math.Pow(this.x - dest.x, 2);
+            var right = Math.Pow(this.y - dest.y, 2);
+
+            return Math.Sqrt(left + right);
+        }
+    }
+}
